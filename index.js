f3b46a47e25cd955d5cbed85bac9b77919f323e8
Jalen Roberts <jalenmr94@hotmail.com>
2019-08-07 14:25:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ function happyHolidays() {
   return ('Happy holidays!');
 }
 
+function happyHolidaysTo(name) {
+  return (`Happy holidays, ${name}!`)
+}
 
-function happyHolidaysTo(holiday, name){
+function happyHolidayTo(holiday, name){
   return (`Happy ${holiday}, ${name}!`);
 }
 
